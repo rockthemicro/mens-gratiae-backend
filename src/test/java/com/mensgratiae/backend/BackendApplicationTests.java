41cc613e87d42c33fec657115031f8f3209613c5
rockthemicro <alex_sontica@yahoo.com>
2020-05-14 22:00:21 +0300

    first commit

---
+++ src/test/java/com/mensgratiae/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensgratiae.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
